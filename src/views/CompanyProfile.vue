b9a825709c4536649b676ea567fb94739f4ec5ea
bagusuntoro <untorobagus65@gmail.com>
2024-01-26 07:37:52 +0700

    bismillah

--- src/views/CompanyProfile.vue
+++ src/views/CompanyProfile.vue
@@ -1,46 +1,49 @@
 <template>
-  <div class="container" style="background-color: #4e73df">
-    <div class="row pt-3">
-      <div class="col-md-6">
-        <h1 style="font-size: 24px; font-weight:bold text-white">ALICORN</h1>
-      </div>
-      <div class="col-md-6">
-        <a href="https://t.me/MrAlicorn" class="float-end text-white">Contact Us</a>
-      </div>
-    </div>
-    <div class="row mt-5">
-      <div class="col-md-6">
-        <h1 style="font-size: 2rem;" class="text-white">ALICORN made you trade easier</h1>
-        <p class="text-white">
-          Alicorn is a 24-hour live signal exclusively for Market Crypto IDX. 
-          <strong style="font-size: 1.2rem;" class="text-white">WHY ALICORN ?</strong>
-          <ul>
-            <li>Easy to use</li>
-            <li>24-hour running</li>
-            <li>NO DELAY</li>
-            <li>OP MANUAL</li>
-            <li>FREE CONSULTATION</li>
-            <li>FAST RESPOND</li>
-            <li>WINRATE INDICATOR &gt; 90%</li>
-          </ul>
-        </p>
-      </div>
-      <div class="col-md-6">
-        <div class="kotak">
-          <img src="/img/logo.jpg" alt="ALc" width="300">
+  <div class="template">
+    <div class="container">
+      <div class="row pt-3">
+        <div class="col-md-6">
+          <h1 style="font-size: 24px; font-weight:bold; font-family: 'Abril Fatface', cursive; color: white;">ALICORN</h1>
+        </div>
+        <div class="col-md-6">
+          <a href="https://t.me/MrAlicorn" class="float-end text-white">Contact Us</a>
         </div>
       </div>
-    </div>
-    <div class="row mt-5 pb-5">
-      <div class="col-md-6">
-        <a href="/login">
-          <button class="btn btn-warning float-end">Login</button>
-        </a>
+      <h1 class="text-center" style="font-family: 'Abril Fatface', cursive; color: white;">ALICORN</h1>
+      <h2 style="font-size: 2rem; font-family: 'Ancient Signboard', cursive; color: white;" class="text-center">made you trade easier</h2>
+      <div class="row mt-5">
+        <div class="col-md-8">
+          <p class="text-white" style="font-family: 'Futura', sans-serif;">
+            Alicorn is a 24-hour live signal exclusively for Market Crypto IDX. <br>
+            <strong style="font-size: 1.2rem; font-family: 'Archive', sans-serif;">WHY ALICORN ?</strong>
+            <ul>
+              <li style="font-family: 'Futura', sans-serif;">Easy to use</li>
+              <li style="font-family: 'Futura', sans-serif;">24-hour running</li>
+              <li style="font-family: 'Futura', sans-serif;">NO DELAY</li>
+              <li style="font-family: 'Futura', sans-serif;">OP MANUAL</li>
+              <li style="font-family: 'Futura', sans-serif;">FREE CONSULTATION</li>
+              <li style="font-family: 'Futura', sans-serif;">FAST RESPOND</li>
+              <li style="font-family: 'Futura', sans-serif;">WINRATE INDICATOR &gt; 90%</li>
+            </ul>
+          </p>
+        </div>
+        <div class="col-md-4">
+          <div class="kotak">
+            <img src="/img/alicornpng.png" alt="ALc" width="300">
+          </div>
+        </div>
       </div>
-      <div class="col-md-6">
-        <a href="/register">
-          <button class="btn btn-success">Register</button>
-        </a>
+      <div class="row mt-5 pb-5">
+        <div class="col-md-6">
+          <a href="/login">
+            <button class="btn btn-warning float-end">Login</button>
+          </a>
+        </div>
+        <div class="col-md-6">
+          <a href="/register">
+            <button class="btn btn-success">Register</button>
+          </a>
+        </div>
       </div>
     </div>
   </div>
@@ -48,13 +51,14 @@
 
 <style scoped>
 h1,
+h2,
 p,
 strong,
 li {
   color: white;
 }
 
-.container {
-  border-radius: 0px 0px 60px 0px;
+.template {
+  background-color: #203155;
 }
 </style>
